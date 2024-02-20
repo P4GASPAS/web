0499164ae5ad71cfe6c1baf3ebe5dfb19443256d
Yhestin <yhestin10@gmail.com>
2024-02-20 10:57:40 +0800

    second commit

--- index.php
+++ index.php
@@ -10,35 +10,35 @@
     <link rel="stylesheet" href="./css_js/style.css">
 </head>
 <body>
-    <div class="container mt-4">
-        <div class="card">
+    <div class="container mt-4 mt-lg-5">
+        <div class="card shadow">
             <div class="container">
                 <div class="row d-flex justify-content-center align-items-center">
-                    <div class="col-4">
-                        <form action="index.php" method="post">
-                            <div class="container-fluid text-center mb-5">
+                    <div class="col-md-4 mx-auto">
+                        <form>
+                            <div class="container-fluid text-center my-5 mt-md-0">
                                 <h1 class="title">Log in</h1>
                             </div>
                             <div class="mb-5">
                             <label class="form-label">Username</label>
-                            <input type="username" class="form-control" name="username">
+                            <input type="username" class="form-control">
                             </div>
-                            <div class="mb-5">
+                            <div class="mb-3">
                             <label class="form-label">Password</label>
-                            <input type="password" class="form-control" name="password">
+                            <input type="password" class="form-control">
                             </div>
                             <div class="mb-5 form-check">
                             <input type="checkbox" class="form-check-input">
-                            <label class="form-check-label">Check me out</label>
+                            <label class="form-check-label">Show password</label>
                             </div>
-                            <button class="btn btn-success px-4">Log in</button>
+                            <button class="btn btn-success px-4 mb-5">Log in</button>
                         </form>
                     </div>
-                    <div class="col-1 d-flex justify-content-center">
+                    <div class="col-1 d-flex justify-content-center d-none d-md-block">
                         <div class="vr" style="height: 600px;"></div>
                     </div>
-                    <div class="col-6">
-                        <img class="img-fluid" src="./images/login_img.svg" alt="illustration">
+                    <div class="col-md-6 d-flex justify-content-center text-center d-none d-md-block">
+                        <img class="rounded img-fluid" src="./images/firstAid.svg" alt="illustration">
                     </div>
                 </div>
             </div>
@@ -48,8 +48,3 @@
     <script src="./css_js/js/bootstrap.min.js"></script>
 </body>
 </html>
-
-<?php
-    echo "{$_POST["username"]}<br>"; 
-    echo "{$_POST["password"]}";
-?>
